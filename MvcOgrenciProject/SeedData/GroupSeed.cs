ad9255eb12e0b1b3824787869fb2897de96913a8
Omeragturk <agturk37@gmail.com>
2024-02-28 15:15:29 +0300

    abc

--- MvcOgrenciProject/SeedData/GroupSeed.cs
+++ MvcOgrenciProject/SeedData/GroupSeed.cs
@@ -1,6 +1,15 @@
-﻿namespace MvcOgrenciProject.SeedData
+﻿using System.Text.RegularExpressions;
+
+namespace MvcOgrenciProject.SeedData
 {
     public class GroupSeed
     {
+        public static List<Group> Groups = new List<Group>
+        {
+            new Group() {ID= 1, Name="Group1" },
+            new Group() {ID= 2, Name="Group2" },
+            new Group() {ID= 3, Name="Group3" },
+
+        };
     }
 }
--- MvcOgrenciProject/SeedData/StudentSeed.cs
+++ MvcOgrenciProject/SeedData/StudentSeed.cs
@@ -1,6 +1,18 @@
-﻿namespace MvcOgrenciProject.SeedData
+﻿using MvcOgrenciProject.Entities;
+
+namespace MvcOgrenciProject.SeedData
 {
     public class StudentSeed
     {
+        public static List<Student> Students = new List<Student>
+        {
+            new Student() {ID= 1, Name="Student1", GroupID=1 },
+            new Student() {ID= 2, Name="Student2", GroupID=1 },
+            new Student() {ID= 3, Name="Student3", GroupID=2 },
+            new Student() {ID= 4, Name="Student4", GroupID=2 },
+            new Student() {ID= 5, Name="Student5", GroupID=3 },
+            new Student() {ID= 6, Name="Student6", GroupID=3 },
+
+        };
     }
 }
