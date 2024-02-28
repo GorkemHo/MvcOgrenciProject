27c140b2e8c404305bacc492a5bcbb5e3d0c8bd3
ilkay <arslanilkay06@gmail.com>
2024-02-28 15:41:05 +0300

    vm commit

--- MvcOgrenciProject/ViewModels/GroupVms/GroupStudentListVm.cs
+++ MvcOgrenciProject/ViewModels/GroupVms/GroupStudentListVm.cs
@@ -1,6 +1,22 @@
-﻿namespace MvcOgrenciProject.ViewModels.GroupVms
+﻿using MvcOgrenciProject.Entities;
+using System.Text.RegularExpressions;
+using Group = MvcOgrenciProject.Entities.Group;
+
+namespace MvcOgrenciProject.ViewModels.GroupVms
 {
     public class GroupStudentListVm
     {
+        private readonly Group _group;
+        private readonly List<Student> _students;
+
+        public GroupStudentListVm(List<Student> students, Group group)
+        {
+            _group = group;
+            _students = students;
+        }
+        public int GroupID => _group.Id;
+        public string GroupName => _group.Name;
+
+        public IEnumerable<Student> Students => _students;
     }
 }
--- MvcOgrenciProject/ViewModels/GroupVms/GroupUpdateVm.cs
+++ MvcOgrenciProject/ViewModels/GroupVms/GroupUpdateVm.cs
@@ -1,6 +1,15 @@
-﻿namespace MvcOgrenciProject.ViewModels.GroupVms
+﻿using MvcOgrenciProject.Entities;
+
+namespace MvcOgrenciProject.ViewModels.GroupVms
 {
     public class GroupUpdateVm
     {
+        private readonly Group _group;
+        public GroupUpdateVm(Group group)
+        {
+            _group = group;
+        }
+        public int GroupID => _group.Id;
+        public string GroupName => _group.Name;
     }
 }
